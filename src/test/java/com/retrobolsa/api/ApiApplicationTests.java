e03b2455506b29609d371579866ff3bb0d45e5b5
Caio Semblano Silva <semblanocaio@gmail.com>
2026-04-25 11:31:28 -0300

    Initial commit com Spring Boot e Docker Compose

---
+++ src/test/java/com/retrobolsa/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrobolsa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
