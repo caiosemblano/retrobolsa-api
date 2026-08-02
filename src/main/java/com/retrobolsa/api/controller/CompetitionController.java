06920071b98b376c39927126d65290eac1973a96
caiosemblano <semblanocaio@gmail.com>
2026-08-02 18:53:41 -0300

    feat(game): add Competition and Portfolio services and REST controllers

---
+++ src/main/java/com/retrobolsa/api/controller/CompetitionController.java
@@ -0,0 +1,22 @@
+package com.retrobolsa.api.controller;
+
+import com.retrobolsa.api.game.competition.CompetitionService;
+import com.retrobolsa.api.game.dto.CompetitionResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/competitions")
+public class CompetitionController {
+
+    private final CompetitionService competitionService;
+
+    @GetMapping("/active")
+    public ResponseEntity<CompetitionResponseDto> getActiveCompetition() {
+        return ResponseEntity.ok(competitionService.getActiveCompetition());
+    }
+}
